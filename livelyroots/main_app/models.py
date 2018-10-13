6bed1b7e1e817c8d810f1b5f390dec9d4eb71bed
Sarah Stensberg <ladystensberg@gmail.com>
2018-10-12 23:27:20 -0700

    added family model

--- livelyroots/main_app/models.py
+++ livelyroots/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Family(models.Model):
+    def get_family_code():
+        return uuid.uuid4()
+
+    family_name = models.CharField(max_length=50)
+    family_code = models.UUIDField(max_length=10, default = get_family_code, editable=False)
+    
+    def __str__(self):
+        return f"Family Name: {self.family_name}, Family Code: {self.family_code}"
