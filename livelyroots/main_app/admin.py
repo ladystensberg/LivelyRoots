fb01589de46699465dc24c0cae85bdf50ea1ecde
Sarah Stensberg <ladystensberg@gmail.com>
2018-10-13 15:56:39 -0700

    added rest of models and their relationships

--- livelyroots/main_app/admin.py
+++ livelyroots/main_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Family
+from .models import Family, Member, Post, Comment
 # Register your models here.
 admin.site.register(Family)
+admin.site.register(Member)
+admin.site.register(Post)
+admin.site.register(Comment)
--- livelyroots/main_app/models.py
+++ livelyroots/main_app/models.py
@@ -10,6 +10,32 @@ class Family(models.Model):
 
     family_name = models.CharField(max_length=50)
     family_code = models.UUIDField(max_length=10, default = get_family_code, editable=False)
-    
+    members = models.ManyToManyField(User)
+
     def __str__(self):
         return f"Family Name: {self.family_name}, Family Code: {self.family_code}"
+
+class Member(models.Model):
+    member = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(blank=True, null=True)
+    location = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return f"{self.member.username}'s birthday is {self.birth_date}. Location is {self.location}."
+
+class Post(models.Model):
+    date = models.DateField()
+    content = models.TextField()
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}. {self.content}. {self.member.username}"
+
+class Comment(models.Model):
+    date = models.DateField()
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}. {self.content}. {self.post}. {self.member.username}"
